1cf747e7e8cd3466d985810c9ddf309a8314d84f
pansolans <94714995+pansolans@users.noreply.github.com>
2023-11-26 21:02:45 -0300

    Update colotto.py

--- colotto.py
+++ colotto.py
@@ -12,28 +12,25 @@ def load_data():
 
 
 def calculate_mahalanobis_distance(Datos_Ejercicio_4, name):
-    # Regularización de la matriz de covarianza
     regularization_factor = 1e-4
     cov_matrix = Datos_Ejercicio_4.drop(columns="Name").cov()
     regularized_cov_matrix = cov_matrix + np.eye(cov_matrix.shape[0]) * regularization_factor
     inv_cov = np.linalg.inv(regularized_cov_matrix)
 
-    # Calcular la distancia de Mahalanobis
     player_data_df = Datos_Ejercicio_4[Datos_Ejercicio_4["Name"] == name].drop(columns="Name")
     player_data = player_data_df.values[0]
     sim = Datos_Ejercicio_4.apply(lambda row: distance.mahalanobis(row.drop("Name").values, player_data, inv_cov), axis=1)
-    
     return sim
 
-# Función principal de Streamlit
+def export_to_excel(df, file_name):
+    df.to_excel(file_name, index=False)
+
 def main():
     df = load_data()
 
-    # Widgets para los filtros
     st.sidebar.header("Pampa Fútbol Analytics")
     st.sidebar.header("Filtros")
 
-    # Filtros pre-búsqueda
     cols_to_select = list(range(23,166))
     selected_vars = st.sidebar.multiselect("Seleccionar variables:", options=df.columns[cols_to_select].tolist())
     competition_ids = st.sidebar.multiselect("Seleccionar competitionId:", options=df["competitionId"].unique().tolist())
@@ -41,15 +38,10 @@ def main():
     pos_principales = st.sidebar.multiselect("Seleccionar Pos_principal:", options=df["Pos_principal"].unique().tolist())
     min_minutes, max_minutes = st.sidebar.slider("Seleccionar rango de minutesOnField:", int(df["minutesOnField"].min()), int(df["minutesOnField"].max()), (int(df["minutesOnField"].min()), int(df["minutesOnField"].max())))
 
-    if not hasattr(st.session_state, 'merged_df'):
-        st.session_state.merged_df = None
-
-    # Si no se ha seleccionado nada en los filtros, mostrar una advertencia y no continuar
     if not selected_vars or not competition_ids or not season_ids or not pos_principales:
         st.warning("Por favor, selecciona opciones en todos los filtros para continuar.")
         return
 
-    # Filtrar DataFrame según las selecciones
     filtered_df = df[
         (df["competitionId"].isin(competition_ids)) & 
         (df["season"].isin(season_ids)) & 
@@ -58,14 +50,12 @@ def main():
         (df["minutesOnField"] <= max_minutes)
     ]
 
-    # Filtro para seleccionar un Jugador
     name = st.sidebar.selectbox("Seleccionar Jugador:", options=[None] + filtered_df["Jugador"].unique().tolist())
     if not name:
         st.warning("Por favor, selecciona un jugador para continuar.")
         return
 
     if st.sidebar.button("Búsqueda"):
-        # Procesamiento
         Datos_Ejercicio_4 = filtered_df[selected_vars]
         Datos_Ejercicio_4["Name"] = filtered_df["Jugador"]
         Datos_Ejercicio_4.fillna(0, inplace=True)
@@ -83,21 +73,22 @@ def main():
         sim_filtered = sim_sorted[["Name", "Sim_Euc_OP_Norm_mah"]]
         merged_df = pd.merge(sim_filtered, filtered_df[["Jugador", "Edad", "urlImagen.y", "equipoActual_nombre", "urlImagen.x", "competitionId", "minutesOnField"]], left_on="Name", right_on="Jugador", how="left")
         st.session_state.merged_df = merged_df
+        st.session_state.displayed_df = None
 
     st.header("Jugadores Similares")
 
-    # Filtros post-búsqueda
     min_age, max_age = st.slider("Minutos jugados:", int(df["minutesOnField"].min()), int(df["minutesOnField"].max()), (int(df["minutesOnField"].min()), int(df["minutesOnField"].max())))
     post_search_competition_ids = st.multiselect("Seleccionar competitionId para filtrar la tabla:", options=st.session_state.merged_df["competitionId"].unique().tolist())
 
     if st.session_state.merged_df is not None:
         filtered_by_age = st.session_state.merged_df[(st.session_state.merged_df["minutesOnField"] >= min_age) & (st.session_state.merged_df["minutesOnField"] <= max_age)]
-        
+
         if post_search_competition_ids:
             filtered_by_age = filtered_by_age[filtered_by_age["competitionId"].isin(post_search_competition_ids)]
 
-        # Visualización con imágenes
-        for index, row in filtered_by_age.head(20).iterrows():
+        st.session_state.displayed_df = filtered_by_age.head(20)  # Almacena solo los primeros 20 registros
+
+        for index, row in st.session_state.displayed_df.iterrows():
             col1, col2, col3 = st.columns([1,3,1])
             if isinstance(row["urlImagen.y"], str):
                 col1.image(row["urlImagen.y"], width=50)
@@ -107,6 +98,13 @@ def main():
             col3.markdown(f"<span style='font-weight: bold;'>Equipo:</span> {row['equipoActual_nombre']}", unsafe_allow_html=True)
             col3.markdown(f"<span style='font-weight: bold;'>Similitud:</span> {row['Sim_Euc_OP_Norm_mah']:.2f}", unsafe_allow_html=True)
             col3.markdown(f"<span style='font-weight: bold; color: red;'>Minutos:</span> {row['minutesOnField']}", unsafe_allow_html=True)
+
+    # Botón para exportar a Excel
+    if st.session_state.displayed_df is not None:
+        if st.button('Exportar a Excel'):
+            file_name = 'jugadores_similares.xlsx'  # Define el nombre del archivo aquí
+            export_to_excel(st.session_state.displayed_df, file_name)
+            st.success(f'Archivo exportado como {file_name}')
 
 if __name__ == "__main__":
     main()
